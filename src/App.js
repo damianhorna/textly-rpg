f847babf7f1c8537d73557fdbb4b7da67272272a
damian-horna <damian.horna@interia.pl>
2019-06-05 19:28:00 +0200

    basic flow working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './App.css';
+import scenario from './scenario.json';
 
-function App() {
-    return (
-        <div className="App">
-            <div className="page-content">
-                <div className="question">Hello, what's your name?</div>
-                <div className="answer">Jon Snow</div>
-                <div className="answer">Arya Stark</div>
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            current: "q1"
+        }
+    }
+
+    loadNext(q){
+        this.setState({
+            current: q
+        })
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <div className="page-content">
+                    <div className="question">{scenario[this.state.current].question}</div>
+                    {scenario[this.state.current].answers.map((a,k) => <div key={k} onClick={() => this.loadNext(a.next)} className="answer">{a.text}</div>)}
+                </div>
             </div>
-
-
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
