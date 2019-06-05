66748fb15ad72ac56153b28b6341af2a1aa0ac03
damian-horna <damian.horna@interia.pl>
2019-06-05 18:41:27 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-content" >
+        <div className="question">question</div>
+        <div className="answer">answer1</div>
+        <div>answer2</div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
